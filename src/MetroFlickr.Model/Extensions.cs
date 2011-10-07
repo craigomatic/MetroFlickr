2268e5098a1d60b62b2131ed3ded34f3e84db172
Craig Presti <capresti@gmail.com>
2011-10-07 16:19:12 -0400

    Added extensions class to MetroFlickr.Model project

---
+++ src/MetroFlickr.Model/Extensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetroFlickr.Model
+{
+    public static class Extensions
+    {
+        public static IEnumerable<FlickrImage> SelectAllImages(this FlickrDataSource datasource)
+        {
+            return datasource.ImageSets.SelectMany(s => s.Collection).ToList();
+        }
+    }
+}

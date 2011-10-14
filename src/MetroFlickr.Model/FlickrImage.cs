cf6025310e66f5ab0cb0db9c7759714ea8e04cd8
Craig Presti <capresti@gmail.com>
2011-10-14 16:34:56 -0400

    Added Date, ImageUri, LargeImageUri properties to FlickrImage

--- src/MetroFlickr.Model/FlickrImage.cs
+++ src/MetroFlickr.Model/FlickrImage.cs
@@ -11,36 +11,40 @@ namespace MetroFlickr.Model
 {
     public class FlickrImage : NotificationObject
     {
+        public DateTime Date { get; private set; }
+
         public string Title { get; set; }
 
         public ImageSource Image { get; private set; }
 
-        private string _LargeImageUri;
-        private string _SmallImageUri;
+        public string ImageUri { get; private set; }
+
+        public string LargeImageUri { get; private set; }
 
         public ImageSource LargeImage
         {
             get
             {
-                return new BitmapImage(new Uri(_LargeImageUri));
+                return new BitmapImage(new Uri(this.LargeImageUri));
             }
         }
 
         public FlickrImageSet ImageSet { get; private set; }
 
-        public FlickrImage(FlickrImageSet imageSet, string smallImageUri, string largeImageUri, string title)
+        public FlickrImage(FlickrImageSet imageSet, string smallImageUri, string largeImageUri, string title, DateTime date)
         {
-            _SmallImageUri = smallImageUri;
-            _LargeImageUri = largeImageUri;
+            this.ImageUri = smallImageUri;
+            this.LargeImageUri = largeImageUri;
 
             this.ImageSet = imageSet;
             this.Image = new BitmapImage(new Uri(smallImageUri));
             this.Title = title;
+            this.Date = date;
         }
 
         public override string ToString()
         {
-            return string.Format("[Img] {0} - {1}", _SmallImageUri, this.Title);
+            return string.Format("[Img] {0} - {1}", this.ImageUri, this.Title);
         }        
     }
 }
